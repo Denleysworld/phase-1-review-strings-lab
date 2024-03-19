9f14e9c77d24de33521c97581c545e0e7874eda4
Denleysworld <deekayel05@gmail.com>
2024-03-19 06:48:58 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Denley Lagat';
+const welcomeMessage = `"Welcome to Flatbook, " ${currentUser}!`;
+const excitedWelcomeMessage =welcomeMessage.toUpperCase()
+const shortGreeting = `"Welcome, "${currentUser.slice(0, 1)}!`;
